7870147d9b4532a3dc55fd941c37e22f4d4bb65b
Emanuel Pontoni <pontoni.emanuel@gmail.com>
2021-08-15 17:51:05 -0300

    Timer

--- js/index.js
+++ js/index.js
@@ -1,26 +1,39 @@
-/*var transform_styles = ['-webkit-transform',
-                        '-ms-transform',
-                        'transform'];
-window.randomize = function() {
- var rotation = Math.floor(Math.random() * 180);
- fill_rotation = rotation / 2;
- document.querySelector('.circle .fill').style.transform = `rotate(${fill_rotation}deg)`;
-}*/
+import { IntervalTimer } from './timer.js';
 
 const numb = document.querySelector('.numb')
 let counter = 1500; /* 1500 seg = 25 min */
 
-setInterval(()=>{
-    if(counter == 0) {
-        clearInterval()
-    } else {
-        counter -= 1
-        numb.textContent = `${Math.trunc(counter/60)}:${ (counter%60 < 10) && (counter%60 >= 0) ? '0' + counter%60 : counter%60}`;
-    }
-}, 1000)
+let timer;
 
-/*let outer = document.querySelector('.outer')
-let inner = document.querySelector('.inner')
-let outer = document.querySelector('.outer')
-let outer = document.querySelector('.outer')*/
+const btnStart = document.querySelector('#start')
+const btnPause = document.querySelector('#pause')
+
+btnStart.addEventListener('click', handleStart)
+btnPause.addEventListener('click', handlePause)
+
+function handleStart(){
+    console.log('Comenzanod el timer....')
+    timer = new IntervalTimer(function () {
+        if(counter == 0) {
+            clearInterval()
+        } else {
+            counter -= 1
+            numb.textContent = `${Math.trunc(counter/60)}:${ (counter%60 < 10) && (counter%60 >= 0) ? '0' + counter%60 : counter%60}`;
+        }
+    }, 1000);
+    btnPause.disabled = false
+    this.disabled = true
+    document.querySelector('.circle .left .progress').style.animationPlayState = 'running'
+    document.querySelector('.circle .right .progress').style.animationPlayState = 'running'
+    document.querySelector('.circle .dot').style.animationPlayState = 'running'
+}
+
+function handlePause(){
+    timer.pause()
+    btnPause.disabled = true
+    btnStart.disabled = false
+    document.querySelector('.circle .left .progress').style.animationPlayState = 'paused'
+    document.querySelector('.circle .right .progress').style.animationPlayState = 'paused'
+    document.querySelector('.circle .dot').style.animationPlayState = 'paused'
+}
 
